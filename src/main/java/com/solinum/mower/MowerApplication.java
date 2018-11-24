af8313397dbbf592b686200091caffed1f6328ca
chetouimohamedaziz <chetoui.mohamedaziz@outlook.fr>
2018-11-24 03:06:28 +0100

    Initial commit
    Initialisation du projet

---
+++ src/main/java/com/solinum/mower/MowerApplication.java
@@ -0,0 +1,12 @@
+package com.solinum.mower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MowerApplication.class, args);
+    }
+}
