67dbe3b6ff4280ef715176f0f0f3e1f30299cded
chetouimohamedaziz <chetoui.mohamedaziz@outlook.fr>
2018-11-25 18:53:38 +0100

    Optimisation
    Optimisation of OrderService

--- src/main/java/com/solinum/mower/OrderService.java
+++ src/main/java/com/solinum/mower/OrderService.java
@@ -28,91 +28,22 @@ public class OrderService {
                 '}';
     }
 
-    public Enum<Direction> changeDirection(char direction, Enum<Direction> initDirection){
-        if (direction == 'D'){
-            switch (initDirection.toString()) {
-                case "N":
-                    initDirection = Direction.E;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-                case "E":
-                    initDirection = Direction.S;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-                case "S":
-                    initDirection = Direction.W;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-                case "W":
-                    initDirection = Direction.N;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-            }
-        } else if (direction == 'G'){
-            switch (initDirection.toString()){
-                case "N":
-                    initDirection = Direction.W;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-                case "W":
-                    initDirection = Direction.S;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-                case "S":
-                    initDirection = Direction.E;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-                case "E":
-                    initDirection = Direction.N;
-                    logger.info("Newest initDirection=" + initDirection);
-                    break;
-            }
-        }
-        return initDirection;
-    }
-
-    public Mower nextStep(int topRight, int lowerLeft, Mower mower){
-        switch (mower.getInitDirection().toString()){
-            case "N":
-                if (mower.getOrd()>=0){
-                    mower.setOrd(mower.getOrd()+1);
-                }
-                logger.info(mower.toString());
-                break;
-            case "S":
-                if (mower.getOrd()<lowerLeft){
-                    mower.setOrd(mower.getOrd()-1);
-                }
-                logger.info(mower.toString());
-                break;
-            case "W":
-                if (mower.getAbs()>=0){
-                    mower.setAbs(mower.getAbs()-1);
-                }
-                logger.info(mower.toString());
-                break;
-            case "E":
-                if (mower.getAbs()<topRight){
-                    mower.setAbs(mower.getAbs()+1);
-                }
-                logger.info(mower.toString());
-                break;
-        }
-        return mower;
-    }
-
-    public Mower executeOrder(OrderRequest orderRequest){
+    public void setOrder(OrderRequest orderRequest){
         mower = new Mower(orderRequest.mower.posAbs, orderRequest.mower.posOrd, Direction.valueOf(orderRequest.mower.dir));
         this.orders = orderRequest.orders;
         grass = new Grass(orderRequest.grass.topRight, orderRequest.grass.lowerLeft);
+    }
+
+    public Mower executeOrder(OrderRequest orderRequest){
+        setOrder(orderRequest);
         logger.info(this.toString());
         for (int i=0; i<this.orders.length(); i++){
             if (this.orders.charAt(i) == 'G' || this.orders.charAt(i) == 'D'){
                 // Change Direction of th mower
-                mower.setInitDirection(changeDirection(this.orders.charAt(i), mower.getInitDirection()));
+                mower.changeDirection(this.orders.charAt(i));
             } else if (orderRequest.orders.charAt(i) == 'A'){
                 // Advance the mower
-                mower = nextStep(grass.getTopRight(), grass.getLowerLeft(), mower);
+                mower.nextStep(grass.getTopRight(), grass.getLowerLeft());
             }
         }
         return mower;
--- src/main/java/com/solinum/mower/dto/Mower.java
+++ src/main/java/com/solinum/mower/dto/Mower.java
@@ -1,6 +1,10 @@
 package com.solinum.mower.dto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Mower extends Coordinate {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private Enum<Direction> initDirection;
 
     public Mower(int abs, int ord, Enum<Direction> initDirection) {
@@ -22,5 +26,76 @@ public class Mower extends Coordinate {
                 super.toString() +
                 ", initDirection=" + initDirection +
                 '}';
+    }
+
+    public void changeDirection(char direction){
+        if (direction == 'D'){
+            switch (this.getInitDirection().toString()) {
+                case "N":
+                    this.setInitDirection(Direction.E);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+                case "E":
+                    this.setInitDirection(Direction.S);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+                case "S":
+                    this.setInitDirection(Direction.W);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+                case "W":
+                    this.setInitDirection(Direction.N);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+            }
+        } else if (direction == 'G'){
+            switch (this.getInitDirection().toString()){
+                case "N":
+                    this.setInitDirection(Direction.W);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+                case "W":
+                    this.setInitDirection(Direction.S);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+                case "S":
+                    this.setInitDirection(Direction.E);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+                case "E":
+                    this.setInitDirection(Direction.N);
+                    logger.info("Newest initDirection=" + this.initDirection);
+                    break;
+            }
+        }
+    }
+
+    public void nextStep(int topRight, int lowerLeft){
+        switch (this.getInitDirection().toString()){
+            case "N":
+                if (this.getOrd()>=0){
+                    this.setOrd(this.getOrd()+1);
+                }
+                logger.info(this.toString());
+                break;
+            case "S":
+                if (this.getOrd()<lowerLeft){
+                    this.setOrd(this.getOrd()-1);
+                }
+                logger.info(this.toString());
+                break;
+            case "W":
+                if (this.getAbs()>=0){
+                    this.setAbs(this.getAbs()-1);
+                }
+                logger.info(this.toString());
+                break;
+            case "E":
+                if (this.getAbs()<topRight){
+                    this.setAbs(this.getAbs()+1);
+                }
+                logger.info(this.toString());
+                break;
+        }
     }
 }
